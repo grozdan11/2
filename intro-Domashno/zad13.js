d690cfc4ac8e5de9a6429616f73ed5329d740c02
grozdan11 <117470@students.ue-varna.bg>
2025-04-01 14:40:06 +0300

    Update zad13.js

--- intro-Domashno/zad13.js
+++ intro-Domashno/zad13.js
@@ -1,1 +1,10 @@
-//not resolved
+function calculateDistance([V1, V2, T]) {
+    let timeInHours = T / 3600.00;
+    let s1 = V1 * timeInHours;
+    let s2 = V2 * timeInHours;
+    let distanceKM = Math.abs(s1 - s2);
+    let distanceM = distanceKM * 1000;
+    console.log(distanceM);
+}
+calculateDistance([55, 33, 2200])
+calculateDistance([30,20,8100])
