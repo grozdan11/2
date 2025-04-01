7e85653d5748ee5db83fd7ad206b336a41a996e0
grozdan11 <117470@students.ue-varna.bg>
2025-04-01 14:29:14 +0300

    Update zad2.js

--- intro-Domashno/zad2.js
+++ intro-Domashno/zad2.js
@@ -1,18 +1,17 @@
-function solve(arr) {
+function calculateSumAndVAT(input) {
     let sum = 0;
 
-    for (const element of arr) {
-        sum += Number(element);
+    for (let i = 0; i < input.length; i++) {
+        sum += Number(input[i]);
     }
 
-    console.log({sum});
-
     let vat = sum * 0.2;
-    console.log({vat});
-    
     let total = sum + vat;
-    console.log({total});
-    
-}
 
-solve(['25']);
+    console.log(`Sum: ${sum.toFixed(2)}`);
+    console.log(`VAT: ${vat.toFixed(2)}`);
+    console.log(`Total: ${total.toFixed(2)}`);
+}
+calculateSumAndVAT(['4', '2.5', '1.6'])
+calculateSumAndVAT(['6', '1.5', '4.4', '1.32', '7.2'])
+calculateSumAndVAT(['7', '3.45', '5.07', '6', '3.54'])
