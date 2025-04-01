a620c611bc3b3af6dfa26a3367234992e7fb76bc
grozdan11 <117470@students.ue-varna.bg>
2025-04-01 14:29:50 +0300

    Update zad3.js

--- intro-Domashno/zad3.js
+++ intro-Domashno/zad3.js
@@ -1,15 +1,11 @@
-function solve(arr) {
-    let counter = 0;
-    let [string, letter] = arr;
-
-    for (const element of string) {
-        if (element == letter) {
-            counter++;
-        }
+function countOccurrences(string, char) {
+    let count = 0;
+    for (let x = 0; x < string.length; x++) {
+        if (string[x] == char) {
+            count++;
+        } 
     }
-
-    console.log({counter});
+    return count;
 }
 
-solve(['hello', 'l']);
-solve(['Bulgaria', 'a']);
+console.log(countOccurrences('hellLo world', 'l'));
